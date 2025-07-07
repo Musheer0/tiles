d6b554ae0e68ed649a456c3c644a0b8674975e79
Musheer0 <147691647+Musheer0@users.noreply.github.com>
2025-07-07 12:10:43 +0530

    trpc intial

---
+++ src/app/(root)/test/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { useTrpc } from '@/trpc/client'
+import { useQuery } from '@tanstack/react-query'
+import React from 'react'
+
+const page = () => {
+    const trcp = useTrpc
+    const {data} = useQuery({
+        queryKey: ['test'],
+        queryFn:async()=>await trcp.hello.query({text: 'wolrd'})
+    });
+  return (
+    <div>
+        {JSON.stringify(data)}
+    </div>
+  )
+}
+
+export default page
